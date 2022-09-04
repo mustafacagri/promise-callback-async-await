f37f9f147e0fc24f748ee731e3c999d69d9a2bff
Mustafa Çağrı Güven <mustafacagri@users.noreply.github.com>
2022-09-04 22:55:54 +0300

    feat: implement the async - await example

---
+++ js/async-await.js
@@ -0,0 +1,33 @@
+const apiURL = 'https://jsonplaceholder.typicode.com/users/' // our url for rest api and we will use ids as a suffix
+
+const fetchAPI = async () => {
+  const response = await axios(apiURL) // we fetch the url here and it will return an array which contains 10 users
+  const data = await response.data // we use the 'await' here again because we do not know defining the data variable how many milliseconds will take
+
+  await data.forEach(user => {
+    // we are using await again because before we populate our table with users data, users data must be ready to use
+    addTheUser(user)
+  })
+
+  userListGenerator()
+}
+
+fetchAPI() // the main execution function
+
+// this approach is not the best usage becauase, js is not a C# or any other backend technologies. and this makes the js is more faster than the others. all works can be handled in the parallel time and we do not need to wait the next job to be handled.
+
+function addTheUser(user) {
+  console.log(user)
+  // this function is not important for our example. we use this to retrieve the data from the rest api in the correct form
+  try {
+    if (user) {
+      const { id, name, email, gender = 'Male' } = user
+      const fullName = name.split(' ')
+      const firstName = fullName[0]
+      const lastName = fullName[1]
+      users.push({ id, firstName, lastName, email, gender })
+    }
+  } catch (error) {
+    console.log(error.message)
+  }
+}
